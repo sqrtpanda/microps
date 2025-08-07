9d1a23a39db40ca30b2dce50a4cf577399bcb3bf
panda <sqrtpanda@gmail.com>
2025-08-07 06:41:39 +0000

    complete Day3-STEP12

--- driver/dummy.h
+++ driver/dummy.h
@@ -2,4 +2,5 @@
 #define DUMMY_H
 
 struct net_device *dummy_init(void);
+
 #endif
---
+++ ether.h
@@ -0,0 +1,30 @@
+#ifndef ETHER_H
+#define ETHER_H
+
+#define ETHER_ADDR_LEN 6
+#define ETHER_ADDR_STR_LEN 18
+
+#define ETHER_HDR_SIZE 14
+#define ETHER_FRAME_SIZE_MIN 60   /* without FCS */
+#define ETHER_FRAME_SIZE_MAX 1514 /* without FCS */
+#define ETHER_PAYLOAD_SIZE_MIN (ETHER_FRAME_SIZE_MIN - ETHER_HDR_SIZE)
+#define ETHER_PAYLOAD_SIZE_MAX (ETHER_FRAME_SIZE_MAX - ETHER_HDR_SIZE)
+
+#define ETHER_TYPE_IP 0x0800
+#define ETHER_TYPE_ARP 0x0806
+#define ETHER_TYPE_IPV6 0x86dd
+
+extern const uint8_t ETHER_ADDR_ANY[ETHER_ADDR_LEN];
+extern const uint8_t ETHER_ADDR_BROADCAST[ETHER_ADDR_LEN];
+
+int ether_addr_pton(const char *p, uint8_t *n);
+char *ether_addr_ntop(const uint8_t *n, char *p, size_t size);
+
+typedef ssize_t (*ether_transmit_func_t)(struct net_device *dev, const uint8_t *data, size_t len);
+typedef ssize_t (*ether_input_func_t)(struct net_device *dev, uint8_t *buf, size_t size);
+
+int ether_transmit_helper(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst, ether_transmit_func_t callback);
+int ether_input_helper(struct net_device *dev, ether_input_func_t callback);
+void ether_setup_helper(struct net_device *dev);
+
+#endif
