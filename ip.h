9cc00f254846cfe5b9e20ef228c11c8a9f0f4b23
panda <sqrtpanda@gmail.com>
2025-08-07 16:56:18 +0000

    complete Day3-STEP17

--- ip.h
+++ ip.h
@@ -38,9 +38,14 @@ struct ip_iface
     ip_addr_t broadcast;
 };
 
+extern const ip_addr_t IP_ADDR_ANY;
+extern const ip_addr_t IP_ADDR_BROADCAST;
+
 void ip_dump(const uint8_t *data, size_t len);
 int ip_addr_pton(const char *p, ip_addr_t *n);
 char *ip_addr_ntop(ip_addr_t n, char *p, size_t size);
+int ip_route_set_default_gateway(struct ip_iface *iface, const char *gateway);
+struct ip_iface *ip_route_get_iface(ip_addr_t dst);
 ssize_t ip_output(uint8_t protocol, const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst);
 struct ip_iface *ip_iface_alloc(const char *unicast, const char *netmask);
 int ip_iface_register(struct net_device *dev, struct ip_iface *iface);
