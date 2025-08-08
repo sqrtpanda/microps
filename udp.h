c0fb0e95d48bf1e26462844179d357d020d9ddb4
panda <sqrtpanda@gmail.com>
2025-08-08 02:26:48 +0000

    complete Day4-STEP20

--- udp.h
+++ udp.h
@@ -7,6 +7,8 @@ ssize_t udp_output(struct ip_endpoint *src, struct ip_endpoint *dst, const uint8
 int udp_init(void);
 int udp_open(void);
 int udp_bind(int index, struct ip_endpoint *local);
+ssize_t udp_sendto(int id, uint8_t *data, size_t len, struct ip_endpoint *foreign);
+ssize_t udp_recvfrom(int id, uint8_t *buf, size_t size, struct ip_endpoint *foreign);
 int udp_close(int id);
 
 #endif
