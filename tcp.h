f235ba5be43ea0e7ea53c6950d26109f731e2b76
panda <sqrtpanda@gmail.com>
2025-08-09 14:19:19 +0000

    complete Day4-STEP25

--- tcp.h
+++ tcp.h
@@ -5,6 +5,8 @@
 
 int tcp_init(void);
 int tcp_open_rfc793(struct ip_endpoint *local, struct ip_endpoint *foreign, int active);
+ssize_t tcp_send(int id, uint8_t *data, size_t len);
+ssize_t tcp_receive(int id, uint8_t *buf, size_t size);
 int tcp_close(int id);
 
 #endif
